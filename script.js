24f924657bc1101de8f31b737e495b799625bafc
Matthew <matthew41305@gmail.com>
2024-11-20 22:47:32 -0800

    added another event listener for submit, stopped default action, added constant for colorValue to equal color input, logs it, and saves it

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // waits for tyhe DOM content to load before it runs the script
 document.addEventListener('DOMContentLoaded', () => {
+
     //gets form of element by the ID
     const form = document.getElementById('colorForm');
     // gets the color of the input by its ID
@@ -11,3 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Saved color: ${savedColor}'); //logs the saved color
         colorInput.value = savedColor; // set the Color input field to the saved value
     }
+
+    // add an event listener to the form for "submit"
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // stops default action for submit
+
+        const colorValue = colorInput.Value; // gets the current value of color input
+        console.log('Selected Color: ${ColorValue}'); // logs selected color to the console
+        // saves the color that its on to local sotrage with 'selectedColor'
+        localStorage.setItem('selectedColor', colorValue);
+    });
+});
